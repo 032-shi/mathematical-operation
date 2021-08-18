c1dec56801a31ec3dddfee4910b97ad4d4d1f941
032-shi <0324omitsu@gmail.com>
2021-08-18 16:24:41 +0900

    [add]変数の設定テスト追加

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 # coding: UTF-8
+# ターミナルに平方根などの特殊記号が出力されないため、Googlecolabを使用する
 import math
-print(math.sqrt(4))
-print(math.sqrt(6))
+import sympy
+
+#print(math.sqrt(4))
+#print(math.sqrt(6))
+
+x, y = sympy.symbols("x y") #変数の設定
+ex = x + 2 * y
+print(ex)
