a9d844772c9e69a59106b1eac74a08be3cb2fa4b
032-shi <0324omitsu@gmail.com>
2021-08-20 14:37:28 +0900

    [start]方程式の処理コーディング開始

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 # coding: UTF-8
+# ターミナルの表示ではなく、googlecolabでの出力を確認する
 import sympy
-print(sympy.sqrt(4))
-print(sympy.sqrt(8))
+#print(sympy.sqrt(4))
+#print(sympy.sqrt(8))
+
+#方程式の処理
+x, y = sympy.symbols("x y")
+test = sympy.Eq(x, y) #方程式"x=y"を定義する
+print(test)
