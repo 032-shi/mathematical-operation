a0395a63c997fe52ac2a1de7a2c05483ce6d6c52
032-shi <0324omitsu@gmail.com>
2021-08-21 17:09:26 +0900

    [add]2次方程式の処理の確認追加

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import sympy
 #print(sympy.sqrt(4))
 #print(sympy.sqrt(8))
 
-#方程式の処理
+#2次方程式の処理
 x, y = sympy.symbols("x y")
-sympy.Eq(x ** 2, 1) #Eq関数で"Xの2乗=1"という方程式を定義する
-#solve関数の第一引数の方程式について、第二引数の変数について解くことができる
-test = sympy.solve(sympy.Eq(x ** 2, 1), x)
+sympy.Eq(x ** 2 + 3 * x - 5, 0)
+test = sympy.solve(sympy.Eq(x ** 2 + 3 * x - 5, 0), x)
 print(test)
+
--- test.py
+++ test.py
@@ -15,3 +15,10 @@ exx = x * ex
 print(exx) #結果が式が展開されずに表示される
 expand_exx = sympy.expand(exx) #式展開する場合は、"expand関数"を使用する
 print(expand_exx)
+
+#方程式の処理
+x, y = sympy.symbols("x y")
+sympy.Eq(x ** 2, 1) #Eq関数で"Xの2乗=1"という方程式を定義する
+#solve関数の第一引数の方程式について、第二引数の変数について解くことができる
+test = sympy.solve(sympy.Eq(x ** 2, 1), x)
+print(test)
