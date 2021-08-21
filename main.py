e59f0ed0683da9e124e837632431b5cda2030d15
032-shi <0324omitsu@gmail.com>
2021-08-21 16:30:47 +0900

    [add]solve関数の処理追加

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ import sympy
 
 #方程式の処理
 x, y = sympy.symbols("x y")
-test = sympy.Eq(x, y) #方程式"x=y"を定義する
+sympy.Eq(x ** 2, 1) #Eq関数で"Xの2乗=1"という方程式を定義する
+#solve関数の第一引数の方程式について、第二引数の変数について解くことができる
+test = sympy.solve(sympy.Eq(x ** 2, 1), x)
 print(test)
